95ac8fb1692420d83314938e6d3e1da8e4763738
Suzume Nomura <SuzuME@ea.g1e.org>
2025-05-09 12:38:41 +0900

    log_upload_config.go: validateタグ内のomitemptyを除去

--- log_upload_config.go
+++ log_upload_config.go
@@ -3,8 +3,8 @@ package webaccel
 // LogUploadConfig ログアップロード設定
 type LogUploadConfig struct {
 	Bucket          string `json:","`
-	Endpoint        string `json:"," validate:"omitempty,equal=https://s3.isk01.sakurastorage.jp"`
-	Region          string `json:"," validate:"omitempty,equal=jp-north-1"`
+	Endpoint        string `json:"," validate:",equal=https://s3.isk01.sakurastorage.jp"`
+	Region          string `json:"," validate:",equal=jp-north-1"`
 	AccessKeyID     string `json:","`
 	SecretAccessKey string `json:","`
 	Status          string `json:"," validate:"oneof=enabled disabled"`
